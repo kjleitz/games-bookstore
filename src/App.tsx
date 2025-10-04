17e695b8f9dc1b884711908e61a76e2827f0f0c9
Keegan Leitz <kjleitz@gmail.com>
2025-10-04 09:38:24 -0400

    fix height thing

--- src/App.tsx
+++ src/App.tsx
@@ -248,7 +248,7 @@ export function App(): JSX.Element {
       } else if (controls.isCollapsed) {
         wrapperClasses.push("flex-none", "max-h-[45vh]", "overflow-hidden");
       } else {
-        wrapperClasses.push("flex-none", "max-h-[45vh]", "min-h-[45vh]", "overflow-hidden");
+        wrapperClasses.push("flex-1", "min-h-0", "max-h-[45vh]", "overflow-hidden");
       }
       if (panelId === "newAdventure") {
         renderedPanels.push(
